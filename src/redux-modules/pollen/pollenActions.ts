6d8bb596006e659f21cb4e49512ba2296c2eebf4
Eike Wolff <henrik.wolff@tobit.software>
2021-06-16 10:55:12 +0200

    :construction: Import json data

--- src/redux-modules/pollen/pollenActions.ts
+++ src/redux-modules/pollen/pollenActions.ts
@@ -1,9 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { Pollen } from '../../types/pollen';
+import pollen from '../../constants/JSON/pollen.json';
 
 export const loadPollen = createAsyncThunk(
     'pollen/load',
     async (data, { getState, dispatch, rejectWithValue }) => {
-        return [] as Array<Pollen>;
+        return pollen as Array<Pollen>;
     },
 )
