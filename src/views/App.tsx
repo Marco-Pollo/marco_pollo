5c4092d552e682bf1ff5d3badf911374a354d238
Carlotta Uhlenbusch <carlotta@uhlenbusch.de>
2021-06-16 11:36:45 +0200

    :sparkles: Add url routing

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,26 +1,28 @@
-import React, {memo} from 'react';
-// @ts-ignore
-import {Route, Switch} from "react-router-dom";
-import HomeView from "./home-view/HomeView";
-import ResultView from "./result-view/ResultView";
-import SelectView from "./select-view/SelectView";
-import AboutView from "./about-view/AboutView";
+import React, { memo } from 'react';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HomeView from './home-view/HomeView';
+import ResultView from './result-view/ResultView';
+import SelectView from './select-view/SelectView';
+import AboutView from './about-view/AboutView';
 
 const App = () => (
-    <Switch>
-        <Route path="/">
-            <HomeView/>
-        </Route>
-        <Route path="/about">
-            <AboutView/>
-        </Route>
-        <Route path="/select">
-            <SelectView/>
-        </Route>
-        <Route path="/result">
-            <ResultView/>
-        </Route>
-    </Switch>
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/">
+                <HomeView />
+            </Route>
+            <Route path="/about">
+                <AboutView />
+            </Route>
+            <Route path="/select">
+                <SelectView />
+            </Route>
+            <Route path="/result">
+                <ResultView />
+            </Route>
+        </Switch>
+    </BrowserRouter>
 );
 
 App.displayName = 'App';
