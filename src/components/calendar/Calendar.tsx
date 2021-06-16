24fb7993c6fc10fccd51a8913064fc1b97cc6327
Eike Wolff <henrik.wolff@tobit.software>
2021-06-16 12:48:03 +0200

    :construction: WIP

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,11 +1,15 @@
 import React, { FunctionComponent, useState } from 'react';
+import { useSelector } from 'react-redux';
 import DateFnsUtils from '@date-io/date-fns';
 import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
 import './calendar.scss';
+import { selectPollen } from '../../redux-modules/pollen/pollenSelectors';
+import { GetScoreForDay } from '../../utils/pollen';
 
 const Calendar: FunctionComponent<Record<string, never>> = () => {
     const [date, setDate] = useState<MaterialUiPickersDate>(new Date());
+    const pollen = useSelector(selectPollen);
 
     return (
         <div className="pollen-calendar">
@@ -17,6 +21,16 @@ const Calendar: FunctionComponent<Record<string, never>> = () => {
                     onChange={setDate}
                     renderDay={
                         (day, selectedDate, dayInCurrentMonth, dayComponent) => {
+                            const score = GetScoreForDay(pollen.ids.map((id) => pollen.entities[id]!), day as Date);
+                            const v = score.score / score.pollen;
+
+                            if (v < 2)
+                                dayComponent.props.style = { ...dayComponent.props.style, backgroundColor: 'yellow' };
+                            else if (v < 3)
+                                dayComponent.props.style = { ...dayComponent.props.style, backgroundColor: 'orange' };
+                            else
+                                dayComponent.props.style = { ...dayComponent.props.style, backgroundColor: 'red' };
+
                             return dayComponent;
                         }
                     }
--- src/redux-modules/pollen/pollenSelectors.ts
+++ src/redux-modules/pollen/pollenSelectors.ts
@@ -7,6 +7,5 @@ export const selectPollenFetchState = (state: RootState) => state.pollen.fetchSt
 
 export const selectPollenIds = (state: RootState) => state.pollen.ids;
 
-export const selectPollenByCategory = (state: RootState, categoryId: number) =>
-    state.pollen.ids.filter((id) => state.pollen.entities[id]?.categoryId === categoryId);
+export const selectPollenByCategory = (state: RootState, categoryId: number) => state.pollen.ids.filter((id) => state.pollen.entities[id]?.categoryId === categoryId);
 export const selectPollenById = (state: RootState, id: number) => state.pollen.entities[id];
