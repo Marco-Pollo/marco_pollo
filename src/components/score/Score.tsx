9e3954e27dae4d7fd4bb399132ede3cbfeb7d6b0
Eike Wolff <henrik.wolff@tobit.software>
2021-06-16 14:08:29 +0200

    :lipstick: Style score component

--- src/components/score/Score.tsx
+++ src/components/score/Score.tsx
@@ -1,12 +1,24 @@
 import React, { FunctionComponent, useState } from 'react';
 import './score.scss';
+import { Typography } from '@material-ui/core';
 
 const Score: FunctionComponent<Record<string, never>> = () => {
-    const [score, setScore] = useState<number>(0);
+    const [score, setScore] = useState<number>(11);
 
     return (
         <div className="pollen-score">
-            {score}
+            <Typography
+                className="pollen-score_number"
+                variant="h2"
+            >
+                {score}
+            </Typography>
+            <Typography
+                className="pollen-score_text"
+                variant="h5"
+            >
+                Score
+            </Typography>
         </div>
     );
 };
