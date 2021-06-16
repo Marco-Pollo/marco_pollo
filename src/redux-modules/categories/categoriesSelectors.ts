54b1b16835e5bf934f05e9c18bae0c8401dd1ab3
Eike Wolff <henrik.wolff@tobit.software>
2021-06-16 10:36:26 +0200

    :construction: Add category selectors

--- src/redux-modules/categories/categoriesSelectors.ts
+++ src/redux-modules/categories/categoriesSelectors.ts
@@ -3,4 +3,7 @@
 import { RootState } from '../store';
 
 export const selectCategories = (state: RootState) => state.categories;
+export const selectCategoryFetchState = (state: RootState) => state.categories.fetchState;
+
+export const selectCategoryIds = (state: RootState) => state.categories.ids;
 export const selectCategoryById = (state: RootState, id: number) => state?.categories.entities[id];
