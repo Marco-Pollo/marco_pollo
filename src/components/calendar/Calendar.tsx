1f85c8c61d182b0e1607606a2c728322132dd37a
Eike Wolff <henrik.wolff@tobit.software>
2021-06-16 11:20:23 +0200

    :construction: Add material-ui picker package

---
+++ src/components/calendar/Calendar.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from 'react';
+import './calendar.scss';
+
+const Calendar: FunctionComponent<Record<string, never>> = () => {
+    return (
+        <div className='pollen-calendar'>
+
+        </div>
+    );
+};
+
+Calendar.displayName = 'Calendar';
+
+export default Calendar;
