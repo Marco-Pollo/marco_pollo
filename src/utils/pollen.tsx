727a3254c0c17632190799c0455ec01c98ae83d3
Eike Wolff <henrik.wolff@tobit.software>
2021-06-17 10:39:40 +0200

    :lipstick: Rounded borders

--- src/utils/pollen.tsx
+++ src/utils/pollen.tsx
@@ -2,58 +2,79 @@ import { Pollen } from '../types/pollen';
 
 export const GetScoreForDay = (pollen: Array<Pollen>, day: Date): {
     light: {
-        onStart: boolean,
-        onEnd: boolean,
-        isBetween: boolean,
+        onStart: boolean | null,
+        onEnd: boolean | null,
+        isBetween: boolean | null,
     },
-    mild:{
-        onStart: boolean,
-        onEnd: boolean,
-        isBetween: boolean,
+    mild: {
+        onStart: boolean | null,
+        onEnd: boolean | null,
+        isBetween: boolean | null,
     },
-    hard:{
-        onStart: boolean,
-        onEnd: boolean,
-        isBetween: boolean,
+    hard: {
+        onStart: boolean | null,
+        onEnd: boolean | null,
+        isBetween: boolean | null,
     },
 } => {
     const copyDate = new Date(day);
-    copyDate.setHours(12, 0, 0, 0);
-    copyDate.setFullYear(1970, copyDate.getMonth(), copyDate.getDate());
+    copyDate.setUTCHours(12, 0, 0, 0);
+    copyDate.setUTCFullYear(1970, copyDate.getMonth(), copyDate.getDate());
 
-    const lightBorder = { onStart: false, onEnd: false, isBetween: false };
-    const mildBorder = { onStart: false, onEnd: false, isBetween: false };
-    const hardBorder = { onStart: false, onEnd: false, isBetween: false };
+    const lightBorder = {
+        onStart: null as (null | boolean),
+        onEnd: null as (null | boolean),
+        isBetween: null as (null | boolean)
+    };
+    const mildBorder = {
+        onStart: null as (null | boolean),
+        onEnd: null as (null | boolean),
+        isBetween: null as (null | boolean)
+    };
+    const hardBorder = {
+        onStart: null as (null | boolean),
+        onEnd: null as (null | boolean),
+        isBetween: null as (null | boolean)
+    };
 
     pollen.forEach((p) => {
         const light = GetDatePosition(p.times.light.start, p.times.light.end, copyDate);
         const mild = GetDatePosition(p.times.light.start, p.times.light.end, copyDate);
         const hard = GetDatePosition(p.times.light.start, p.times.light.end, copyDate);
-        if (light.onStart) lightBorder.onStart = true;
-        if (light.onEnd) lightBorder.onEnd = true;
+        if (light.onStart && lightBorder.onStart === null) lightBorder.onStart = true;
+        else if (!light.onStart && light.isBetween) lightBorder.onStart = false;
+        if (light.onEnd && lightBorder.onEnd === null) lightBorder.onEnd = true;
+        else if (!light.onEnd && light.isBetween) lightBorder.onEnd = false;
         if (light.isBetween) lightBorder.isBetween = true;
-        if (mild.onStart) mildBorder.onStart = true;
-        if (mild.onEnd) mildBorder.onEnd = true;
+
+        if (mild.onStart && mildBorder.onStart === null) mildBorder.onStart = true;
+        else if (!mild.onStart && light.isBetween) mildBorder.onStart = false;
+        if (mild.onEnd && mildBorder.onEnd === null) mildBorder.onEnd = true;
+        else if (!mild.onEnd && light.isBetween) mildBorder.onEnd = false;
         if (mild.isBetween) mildBorder.isBetween = true;
-        if (hard.onStart) hardBorder.onStart = true;
-        if (hard.onEnd) hardBorder.onEnd = true;
+
+        if (hard.onStart && hardBorder.onStart === null) hardBorder.onStart = true;
+        else if (!hard.onStart && light.isBetween) hardBorder.onStart = false;
+        if (hard.onEnd && hardBorder.onEnd === null) hardBorder.onEnd = true;
+        else if (!hard.onEnd && light.isBetween) hardBorder.onEnd = false;
         if (hard.isBetween) hardBorder.isBetween = true;
     });
 
     return {
         light: lightBorder,
         mild: mildBorder,
-        hard: hardBorder,
+        hard: hardBorder
     };
 };
 const GetDatePosition = (start: string, end: string, date: Date):
     { isBetween: boolean, onStart: boolean, onEnd: boolean } => {
-    const startDate = new Date(start);
-    const endDate = new Date(end);
+    const startDate = new Date(start).getTime();
+    const endDate = new Date(end).getTime();
+    const dateTime = date.getTime();
 
     return {
-        isBetween: date >= startDate && date <= endDate,
-        onEnd: endDate === date,
-        onStart: startDate === date,
+        isBetween: dateTime >= startDate && dateTime <= endDate,
+        onEnd: endDate === dateTime,
+        onStart: startDate === dateTime
     };
 };
