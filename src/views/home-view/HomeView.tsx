f154e9ec6ab76c8cfd2a5eefa934f3a6394a31b5
Carlotta Uhlenbusch <carlotta@uhlenbusch.de>
2021-06-17 09:15:12 +0200

    :sparkles: Add scroll down button

--- src/views/home-view/HomeView.tsx
+++ src/views/home-view/HomeView.tsx
@@ -2,23 +2,38 @@ import React, { memo } from 'react';
 import Header from '../../components/header/Header';
 import { Typography } from '@material-ui/core';
 import './homeView.scss';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import SecondPage from './second-page/SecondPage';
 
-const HomeView = () => (
-    <div className='home-view'>
-        <Header />
-        <div className='home-view__view-port home-view__view-port--home'>
-            <Typography variant='h2' align='center' className='home-view__headline'>
-                MarcoPollo to empower your daily life
-            </Typography>
-        </div>
-        <div className='home-view__view-port'>
-            <div className='home-view__button-container'>
-                <SecondPage />
+const HomeView = () => {
+    const secondPage = React.createRef<HTMLDivElement>();
+
+    return (
+        <div className='home-view'>
+            <Header />
+            <div className='home-view__view-port home-view__view-port--home'>
+                <Typography variant='h2' align='center' className='home-view__headline'>
+                    MarcoPollo to empower your daily life
+                </Typography>
+                <div
+                    onClick={() => {
+                        if (secondPage && secondPage.current) {
+                            secondPage.current.scrollIntoView({ behavior: 'smooth', inline: 'nearest' });
+                        }
+                    }}
+                    className='home-view__view-port--scroll-down'
+                >
+                    <ExpandMoreIcon />
+                </div>
+            </div>
+            <div ref={secondPage} className='home-view__view-port'>
+                <div className='home-view__button-container'>
+                    <SecondPage />
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 HomeView.displayName = 'HomeView';
 
