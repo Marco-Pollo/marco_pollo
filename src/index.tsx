67d6738a0d9ee01d29571526d09b64ce82d02721
Konrad Knauer <konrad.knauer@tobit.com>
2021-06-17 09:00:04 +0200

    :lipstick: Improve scrollbar

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import '@fontsource/roboto/100.css';
 import ReactDOM from 'react-dom';
 import { MuiThemeProvider } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Provider } from 'react-redux';
 import App from './views/App';
 import reportWebVitals from './reportWebVitals';
 import theme from './constants/theme';
-import { Provider } from 'react-redux';
 import store from './redux-modules/store';
 import { loadCategories } from './redux-modules/categories/categoriesActions';
 import { loadPollen } from './redux-modules/pollen/pollenActions';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import './index.css';
+import './index.scss';
 
 const render = () => {
     ReactDOM.render(
