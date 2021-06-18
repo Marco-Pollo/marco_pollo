4827061d0e41cc6e1b6cf21fce3f40fb15e5ab86
Carlotta Uhlenbusch <carlotta@uhlenbusch.de>
2021-06-18 13:30:34 +0200

    :lipstick: Add footer and text

--- src/views/result-view/ResultView.tsx
+++ src/views/result-view/ResultView.tsx
@@ -2,12 +2,16 @@ import React, { memo } from 'react';
 import Header from '../../components/header/Header';
 import Calendar from '../../components/calendar/Calendar';
 import Score from '../../components/score/Score';
+import Footer from '../../components/waves/footer-wave/Footer';
+import ResultText from './result-text/ResultText';
 
 const ResultView = () => (
     <>
         <Header />
+        <ResultText/>
         <Calendar />
         <Score />
+        <Footer/>
     </>
 );
 
